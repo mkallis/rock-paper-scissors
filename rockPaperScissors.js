033eecf1972b5c08773802db8834216bd307d43a
Megan Kallis <megankallis@gmail.com>
2023-06-15 18:15:08 -0700

    Adds method that determines computer choice with random num generator

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,3 +9,17 @@ const getUserChoice = userInput => {
       return 'invalid input'
     } 
 }
+
+// Computer choice uses a random number to select rock, paper, or scissors
+const getComputerChoice = () => {
+    const randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0) {
+    return 'rock';
+    } else if (randomNum === 1) {
+    return 'paper';
+    } else if (randomNum === 2) {
+    return 'scissors';
+    } else {
+    return 'Something went wrong.'
+    }
+}
